b400f5f044bd6a648f99850b05581e8f2cc1e8e4
Pizzacorn.es <hola@pizzacorn.es>
2026-01-14 09:36:36 +0100

    En proceso de la version 11

--- lib/pizzacorn_ui.dart
+++ lib/pizzacorn_ui.dart
@@ -1,19 +1,16 @@
-// appbar
-export 'src/appbars/appbars.dart';
-
 //adapters
 export 'src/adapters/web_adapter.dart';
 export 'src/adapters/web_popup.dart';
 
-// buttons
-export 'src/buttons/buttons.dart';
-export 'src/buttons/button_icon.dart';
-export 'src/buttons/more_menu_button.dart';
-export 'src/buttons/styles.dart';
-export 'src/buttons/pop_up.dart';
+// appbar
+export 'src/appbars/appbars.dart';
 
-//decorations
-export 'src/decorations/decorations.dart';
+// buttons
+export 'src/buttons/button_icon.dart';
+export 'src/buttons/buttons.dart';
+export 'src/buttons/more_menu_button.dart';
+export 'src/buttons/pop_up.dart';
+export 'src/buttons/styles.dart';
 
 // cropper
 export 'src/cropper/crop_page.dart';
@@ -21,12 +18,21 @@ export 'src/cropper/file_model.dart';
 export 'src/cropper/image_picker.dart';
 export 'src/cropper/pressed_image.dart';
 
+//decorations
+export 'src/decorations/decorations.dart';
+
+//dropdown
+export 'src/dropdown/dropdown.dart';
+
 // effects
 export 'src/effects/shimmer.dart';
 export 'src/effects/show_up.dart';
 export 'src/effects/hover.dart';
 
 // form
+export 'src/form/choice_field.dart';
+export 'src/form/relatioin_field.dart';
+export 'src/form/subtitle_field.dart';
 export 'src/form/textfield.dart';
 
 // icons
@@ -38,17 +44,8 @@ export 'src/images/images.dart';
 // layout
 export 'src/layout/space.dart';
 
-// pickers
-export 'src/pickers/icon_picker.dart';
-
-//dropdown
-export 'src/dropdown/dropdown.dart';
-
 //list
 export 'src/list/inifinite_scroll.dart';
-
-//services
-export 'src/services/firebase_pagination.dart';
 
 // navigation
 export 'src/navigation/navigation_helpers.dart';
@@ -58,6 +55,12 @@ export 'src/navigation/overlays_helpers.dart';
 export 'src/overlays/bottom_sheet.dart';
 export 'src/overlays/loading.dart';
 export 'src/overlays/bottom_popups.dart';
+
+// pickers
+export 'src/pickers/icon_picker.dart';
+
+//services
+export 'src/services/firebase_pagination.dart';
 
 // text
 export 'src/text/textstyles.dart';
